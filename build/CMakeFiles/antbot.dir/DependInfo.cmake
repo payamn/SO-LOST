af2167500fe328eb966bba55bbb970f3b4ba0716
payam nikdel <payam222@gmail.com>
2016-12-14 19:59:59 -0800

    complete ant so lost project

---
+++ build/CMakeFiles/antbot.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/payam/git_projects/ant_trails/src/ant_bot.cpp" "/home/payam/git_projects/ant_trails/build/CMakeFiles/antbot.dir/src/ant_bot.cpp.o"
+  "/home/payam/git_projects/ant_trails/src/crumb.cpp" "/home/payam/git_projects/ant_trails/build/CMakeFiles/antbot.dir/src/crumb.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/payam/stg/include/Stage-4.1"
+  "/usr/lib/fltk"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/antbot.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/antbot.dir/src/ant_bot.cpp.o"
+  "CMakeFiles/antbot.dir/src/crumb.cpp.o"
+  "antbot.pdb"
+  "antbot.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/antbot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
